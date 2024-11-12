f9ba7050e12c4c29d000e485d165cebf74731dfc
nakaryunosuke <naka.sava0309@gmail.com>
2024-11-12 12:13:16 +0900

    Add: Serviceクラスの追加。

---
+++ src/main/java/com/example/service/EmployeeService.java
@@ -0,0 +1,42 @@
+package com.example.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.domain.Employee;
+import com.example.repository.EmployeeRepository;
+/**
+ * @author nakaryunosuke
+ */
+@Service
+@Transactional
+public class EmployeeService {
+    @Autowired
+    private EmployeeRepository repository;
+
+    /**
+     * 従業員情報を全件取り出し、入社日順で並べる。
+     * @return 全ての従業員情報を返す。
+     */
+    public List<Employee> findAll(){
+        return repository.findAll();
+    }
+    /**
+     * 主キーに当てはまる従業員情報を検索する。
+     * @param idを受け取る。
+     */
+    public Employee load(Integer id){
+        return repository.load(id);
+    }
+    /**
+     * 従業員情報を更新する。
+     * @param 従業員情報を受け取る。
+     */
+    public void update(Employee employee){
+        repository.update(employee);
+    }
+
+}
