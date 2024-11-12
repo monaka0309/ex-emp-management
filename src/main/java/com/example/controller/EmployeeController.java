0771779a14703c74d0418ea2c08dd1873c2cc14e
nakaryunosuke <naka.sava0309@gmail.com>
2024-11-12 21:00:34 +0900

    Add: コントローラークラスの作成。

---
+++ src/main/java/com/example/controller/EmployeeController.java
@@ -0,0 +1,30 @@
+package com.example.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.domain.Employee;
+import com.example.service.EmployeeService;
+
+/**
+ * @author nakaryunosuke
+ * 従業員のコントローラークラス
+ */
+@Controller
+@RequestMapping("/employee")
+public class EmployeeController {
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping("/showList")
+    public String showList(Model model){
+        List<Employee> employeeList = employeeService.showList();
+        model.addAttribute("employeeList", employeeList);
+        return "employee/list";
+    }
+}
