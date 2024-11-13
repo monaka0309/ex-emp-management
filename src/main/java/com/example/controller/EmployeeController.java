fccfae8f1ce191dc4235c64034a6945846655e9d
nakaryunosuke <naka.sava0309@gmail.com>
2024-11-13 10:34:42 +0900

    Add: EmployeeControllerの作成。

--- src/main/java/com/example/controller/EmployeeController.java
+++ src/main/java/com/example/controller/EmployeeController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.domain.Employee;
+import com.example.form.UpdateEmployeeForm;
 import com.example.service.EmployeeService;
 
 /**
@@ -21,10 +22,27 @@ public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
 
+    /**
+     * 従業員一覧を出力する。
+     * @param リクエストスコープを受け取る。
+     * @return viewファイルを返す。
+     */
     @GetMapping("/showList")
     public String showList(Model model){
         List<Employee> employeeList = employeeService.showList();
         model.addAttribute("employeeList", employeeList);
         return "employee/list";
+    }
+    /**
+     * 従業員詳細を開く。
+     * @param id,form,modelを取得する。
+     * @return viewファイルを返す。
+     */
+    @RequestMapping("/showDetail")
+    public String showDetail(String id, Model model, UpdateEmployeeForm form){
+        Integer num = Integer.parseInt(id);
+        Employee employee = employeeService.showDetail(num);
+        model.addAttribute("employee", employee);
+        return "employee/detail";
     }
 }
